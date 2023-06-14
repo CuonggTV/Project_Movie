d6a5c2dcdbe00e59674c06e262e3026b63903fab
Kayden <nguyendaithinh3004@gami.com>
2023-06-14 20:39:26 +0700

    commit for fun

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello Cuong");
-        System.out.println("Hello Cuong2123123");
-        Scanner sc = new Scanner(System.in);
-        ákjdlhakjsldhlaksdjhflaksjdhfklajsdhfklajsdhflkajshdfkljasdhf
+        System.out.println("okekkkk");
     }
 }
