3509f96b3a669340acfe0a14abcfeb2776adcaf9
CuonggTV <124784766+CuonggTV@users.noreply.github.com>
2023-06-16 20:30:24 +0700

    test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello Cuong");
-        System.out.println("Hello Cuong2123123");
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Day la cuong");
+        int i,k;
+        for(i=1;;i++){
+            k=5;
+        }
+        System.out.println(i);
     }
 }
