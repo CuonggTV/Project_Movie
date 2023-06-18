f92e197d5dbf5cda01527949460e2a6012e31bd9
CuonggTV <124784766+CuonggTV@users.noreply.github.com>
2023-06-18 20:24:39 +0700

    Update MovieList.java, TicketList.java

---
+++ src/core/Admin.java
@@ -0,0 +1,11 @@
+package core;
+
+public class Admin extends User {
+    public Admin(String username, String password, String fullName, String phoneNumber, String email) {
+        super(username, password, fullName, phoneNumber, email);
+    }
+
+    public void createMovie(){
+
+    }
+}
