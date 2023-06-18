cae37e5a2261d74c669fc3fd518f61da8b540037
CuonggTV <124784766+CuonggTV@users.noreply.github.com>
2023-06-18 21:39:37 +0700

    Add createMovie

--- src/core/Admin.java
+++ src/core/Admin.java
@@ -7,27 +7,34 @@ public class Admin extends User {
         super(username, password, fullName, phoneNumber, email);
     }
 
-//    public void createMovie(MovieList mv){
-//        boolean loop = false;
-//        String movieName;
-//        String author;
-//        do{
-//            loop = false;
-//            System.out.flush();
-//            movieName = MyUtil.inputString("Enter movie name: ");
-//            author = MyUtil.inputString("Enter author: ");
-//            showTime = MyUtil.inputString("Enter show time: ");
-//            price = M
-//            for(Movie mvt : mv){
-//                if(mvt.getMovieName().equals(movieName)){
-//                    loop = true;
-//                    System.out.println("This movie is already on air!");
-//                    break;
-//                }
-//            }
-//        }while(loop);
-//
-//
-//
-//    }
+    public void createMovie(MovieList mv){
+        boolean loop = false;
+        String movieName,author,showTime;
+        double price;
+
+        do{
+            loop = false;
+            System.out.flush();
+            movieName = MyUtil.inputString("Enter movie name: ");
+            author = MyUtil.inputString("Enter author: ");
+            showTime = MyUtil.inputString("Enter show time: ");
+            price = MyUtil.inputDouble("Enter ticket price: ");
+
+            //Check movie
+            for(Movie mvt : mv){
+                if(mvt.getMovieName().equals(movieName)) {
+                    loop = true;
+                    System.out.println("This movie is already on air!");
+                    break;
+                }
+            }
+            //Check author
+            if(!author.matches("[a-zA-Z]+")){
+                loop = true;
+                System.out.println("Author just contains alphabet");
+            }
+        }while(loop);
+        Movie newMovie = new Movie(movieName,author,showTime,price);
+        mv.add(newMovie);
+    }
 }
